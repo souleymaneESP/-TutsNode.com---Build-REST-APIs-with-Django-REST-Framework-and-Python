895ee8b442d31819439e0cff6a1c927134deda30
souleymane chs <18301@esp.mr>
2022-05-14 13:25:36 +0000

    4.5 Validation

--- watchmate/watchmate_app/api/serializers.py
+++ watchmate/watchmate_app/api/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 from watchmate_app.models import Movie
 
+
+
+def name_length(value):
+    if len(value)<2:
+        raise serializers.ValidationError("Name is too short")
+    
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name=serializers.CharField()
+    name=serializers.CharField(validators=[name_length])
     description= serializers.CharField()
     active= serializers.BooleanField()
 
@@ -16,3 +23,17 @@ class MovieSerializer(serializers.Serializer):
         instance.active=validated_data.get('active',instance.active)
         instance.save()
         return instance
+
+    def validate(self,data):
+        if data['title']==data['description']:
+            raise serializers.ValidationError("Name and Title should not be similar")
+        return data
+
+
+    # def validate_name(self,value):
+    #     if len(value)<2:
+    #         raise serializers.ValidationError("Name is too short")
+    #     else:
+    #         return value
+
+    
--- watchmate/watchmate_app/api/urls.py
+++ watchmate/watchmate_app/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
 from watchmate_app.api.views import *
 urlpatterns = [
-    path('list/',movie_list,name="movie-list"),
-    path('<int:pk>',movie_detail,name="movie-detail")
+    path('list/',MovieListAV.as_view(),name="movie-list"),
+    path('<int:pk>',MovieDetailAV.as_view(),name="movie-detail")
 ]
