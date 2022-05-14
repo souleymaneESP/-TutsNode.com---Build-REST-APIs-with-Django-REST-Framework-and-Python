645898ac5c0bf5d687211fbf01fe54bc7c6325ba
souleymane chs <18301@esp.mr>
2022-05-14 10:36:20 +0000

    3.5 Creating Json response individual element

--- watchmate/watchmate_app/urls.py
+++ watchmate/watchmate_app/urls.py
@@ -1,7 +1,8 @@
 
 
 from django.urls import path,include
-from watchmate_app.views import movie_list
+from watchmate_app.views import *
 urlpatterns = [
-    path('list/',movie_list,name="movie-list")
+    path('list/',movie_list,name="movie-list"),
+    path('<int:pk>',movie_detail,name="movie-detail")
 ]
--- watchmate/watchmate_app/views.py
+++ watchmate/watchmate_app/views.py
@@ -8,3 +8,13 @@ def movie_list(request):
         'movies': list(movies.values())
     }
     return JsonResponse(data)
+
+
+def movie_detail(request,pk):
+    movie = Movie.objects.get(pk=pk)
+    data={
+        'name': movie.name,
+        'description': movie.description,
+        'active': movie.active
+    }
+    return JsonResponse(data)
