fbb5ed8bbe50c0b90eb929ba37238a7955c9cc45
souleymane chs <18301@esp.mr>
2022-05-14 20:20:03 +0000

    6.3 Temporary Login Logout

--- watchmate/watchmate/urls.py
+++ watchmate/watchmate/urls.py
@@ -4,5 +4,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movie/',include('watchmate_app.api.urls'))
+    path('movie/',include('watchmate_app.api.urls')),
+    path('api-auth/',include('rest_framework.urls'))
 ]
