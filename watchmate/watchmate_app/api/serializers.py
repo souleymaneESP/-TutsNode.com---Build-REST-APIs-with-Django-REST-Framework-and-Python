1f52c9dc56b21d3bb7a144c5835024e7f79ecfc7
souleymane chs <18301@esp.mr>
2022-05-14 14:00:54 +0000

    4.7 Model Serializer

--- watchmate/watchmate_app/api/serializers.py
+++ watchmate/watchmate_app/api/serializers.py
@@ -3,31 +3,62 @@ from watchmate_app.models import Movie
 
 
 
-def name_length(value):
-    if len(value)<2:
-        raise serializers.ValidationError("Name is too short")
+
+
+
+
+class MovieSerializer(serializers.ModelSerializer):
     
-
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name=serializers.CharField(validators=[name_length])
-    description= serializers.CharField()
-    active= serializers.BooleanField()
-
-    def create(self,validated_data):
-        return Movie.objects.create(**validated_data)
-
-    def update(self,instance,validated_data):
-        instance.name=validated_data.get('name',instance.name)
-        instance.description=validated_data.get('description',instance.description)
-        instance.active=validated_data.get('active',instance.active)
-        instance.save()
-        return instance
+     class Meta:
+        model=Movie
+        fields="__all__"
+        # exclude=['active']
 
     def validate(self,data):
         if data['title']==data['description']:
             raise serializers.ValidationError("Name and Title should not be similar")
         return data
+
+    def validate_name(self,value):
+        if len(value)<2:
+            raise serializers.ValidationError("Name is too short")
+        else:
+            return value
+
+
+
+
+
+
+
+
+
+
+# def name_length(value):
+#     if len(value)<2:
+#         raise serializers.ValidationError("Name is too short")
+    
+
+# class MovieSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name=serializers.CharField(validators=[name_length])
+#     description= serializers.CharField()
+#     active= serializers.BooleanField()
+
+#     def create(self,validated_data):
+#         return Movie.objects.create(**validated_data)
+
+#     def update(self,instance,validated_data):
+#         instance.name=validated_data.get('name',instance.name)
+#         instance.description=validated_data.get('description',instance.description)
+#         instance.active=validated_data.get('active',instance.active)
+#         instance.save()
+#         return instance
+
+#     def validate(self,data):
+#         if data['title']==data['description']:
+#             raise serializers.ValidationError("Name and Title should not be similar")
+#         return data
 
 
     # def validate_name(self,value):
